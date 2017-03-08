a877236d7c34e5eca9a9e9e7f40feb5e437e050e
nunomartins <veiga.jardim@gmail.com>
2017-03-08 08:42:30 -0300

    Término do treinamento.

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -6,18 +6,15 @@ import { AppComponent }   from './app.component';
 import { FotoModule } from './foto/foto.module';
 import { PainelModule } from './painel/painel.module';
 import {HttpModule} from '@angular/http';
-
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { ListagemComponent } from './listagem/listagem.component';
-
 import { routing } from './app.router';
-
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports:      [ BrowserModule, FotoModule, HttpModule, PainelModule, routing, FormsModule ],
+  imports:      [ BrowserModule, FotoModule, HttpModule, PainelModule, routing, FormsModule, ReactiveFormsModule ],
   declarations: [ AppComponent, CadastroComponent, ListagemComponent ],
   bootstrap:    [ AppComponent ]
 })
+
  export class AppModule { }
